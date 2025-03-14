8066b2af05c4728d820117a94f2afbe02a0fd901
Hamamul Fauzi <hamamul.fauzi@gmail.com>
2025-03-14 15:08:32 +0700

    title

--- common/models/Product.php
+++ common/models/Product.php
@@ -41,8 +41,8 @@ class Product extends \common\models\CustomActiveRecord
     public function rules()
     {
         return [
-            [['title'], 'required'],
-            [['short_desc', 'content', 'content_id', 'content_en'], 'string'],
+            // [['title'], 'required'],
+            [['title_id', 'title','title_en','short_desc', 'content', 'content_id', 'content_en'], 'string'],
             [['is_published', 'status_active', 'created_by', 'updated_by', 'deleted_by', 'position'], 'integer'],
             [['published_at', 'created_at', 'updated_at', 'deleted_at'], 'safe'],
             [['category', 'title', 'image_url', 'slug', 'path'], 'string', 'max' => 255],
